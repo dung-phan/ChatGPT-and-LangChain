4cee3d30be93676ca29bfb5854b2b1c37f4cc526
dung-phan <dungphan@pm.me>
2025-07-27 00:03:10 +0200

    Adds a chat history to the chain

--- tchat.py
+++ tchat.py
@@ -1,5 +1,7 @@
+from langchain_core.chat_history import InMemoryChatMessageHistory
+from langchain_core.runnables import RunnableWithMessageHistory
 from langchain_openai import ChatOpenAI
-from langchain_core.prompts import HumanMessagePromptTemplate, ChatPromptTemplate
+from langchain_core.prompts import HumanMessagePromptTemplate, ChatPromptTemplate, MessagesPlaceholder
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -9,16 +11,30 @@ chat = ChatOpenAI(
 
 prompt = ChatPromptTemplate.from_messages(
     messages=[
-        HumanMessagePromptTemplate.from_template("{content}"),
+        MessagesPlaceholder("messages"),
+        HumanMessagePromptTemplate.from_template("{input}"),
     ]
 )
+store = {}
+
+def get_history(session_id: str):
+    if session_id not in store:
+        store[session_id] = InMemoryChatMessageHistory()
+    return store[session_id]
 
 chain = prompt | chat
+chat_with_history = RunnableWithMessageHistory(
+    runnable=chain,
+    get_session_history=get_history,
+    input_messages_key="input",
+    history_messages_key="messages",
+)
 
 while True:
     content = input(">> ")
 
-    result = chain.invoke({
-        "content": content,
-    })
+    result = chat_with_history.invoke(
+    {"input": content},
+        config={"configurable": {"session_id": "test_session"}}
+    )
     print(result.content)
