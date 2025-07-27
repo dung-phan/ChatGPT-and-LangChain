140bcabd33d168c0e646ccb20c7aaae10b62b347
dung-phan <dungphan@pm.me>
2025-07-27 21:47:38 +0200

    Uses LCEL to retrieve data from vector store

---
+++ prompt.py
@@ -0,0 +1,35 @@
+import os
+
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_openai import OpenAI
+from langchain_openai import AzureOpenAIEmbeddings
+from langchain_community.vectorstores.chroma import Chroma
+from langchain_core.runnables import RunnablePassthrough
+
+load_dotenv()
+
+embeddings = AzureOpenAIEmbeddings(
+    azure_endpoint=os.getenv('AZURE_ENDPOINT'),
+    deployment=os.getenv('AZURE_DEPLOYMENT_NAME'),
+    api_key=os.getenv('AZURE_API_KEY'),
+)
+db = Chroma(
+    persist_directory="chroma_db",
+    embedding_function=embeddings,
+) # Initialize the Chroma vector store to query the database
+
+llm = OpenAI(model="gpt-4o-mini")
+retriever = db.as_retriever()
+prompt = PromptTemplate.from_template(
+    "Use the following context to answer the question:\n\n{context}\n\nQuestion: {question}"
+)
+qa_chain = ({
+    "context": retriever,
+    "question": RunnablePassthrough(),
+}
+    | prompt
+    | llm
+)
+result = qa_chain.invoke("What is an interesting fact about the English language?")
+print(result)
